389c8e13ea8f12900d72bb567656a055b7efdf7b
Frankenstei <enwereloveday@yahoo.com>
2022-12-21 16:17:26 -0800

    9th commit

--- shell_loop.c
+++ shell_loop.c
@@ -21,10 +21,12 @@ int hsh(info_t *info, char **av)
 		_eputchar(BUF_FLUSH);
 		r = get_input(info);
 		if (r != -1)
+		{
 			set_info(info, av);
 			builtin_ret = find_builtin(info);
 			if (builtin_ret == -1)
 				find_cmd(info);
+		}
 		else if (interactive(info))
 			_putchar('\n');
 		free_info(info, 0);
@@ -34,9 +36,11 @@ int hsh(info_t *info, char **av)
 	if (!interactive(info) && info->status)
 		exit(info->status);
 	if (builtin_ret == -2)
+	{
 		if (info->err_num == -1)
 			exit(info->status);
 		exit(info->err_num);
+	}
 	return (builtin_ret);
 }
 
--- tokenizer.c
+++ tokenizer.c
@@ -18,15 +18,18 @@ char **strtow(char *str, char *d)
 	if (!d)
 		d = " ";
 	for (i = 0; str[i] != '\0'; i++)
+	{
 		if (!is_delim(str[i], d) &&
 				(is_delim(str[i + 1], d) || !str[i + 1]))
 			numwords++;
+	}
 	if (numwords == 0)
 		return (NULL);
 	s = malloc((1 + numwords) * sizeof(char *));
 	if (!s)
 		return (NULL);
 	for (i = 0, j = 0; j < numwords; j++)
+	{
 		while (is_delim(str[i], d))
 			i++;
 		k = 0;
@@ -34,13 +37,16 @@ char **strtow(char *str, char *d)
 			k++;
 		s[j] = malloc((k + 1) * sizeof(char));
 		if (!s[j])
+		{
 			for (k = 0; k < j; k++)
 				free(s[k]);
 			free(s);
 			return (NULL);
+		}
 		for (m = 0; m < k; m++)
 			s[j][m] = str[i++];
 		s[j][m] = 0;
+	}
 	s[j] = NULL;
 	return (s);
 }
@@ -61,15 +67,18 @@ char **strtow2(char *str, char d)
 	if (str == NULL || str[0] == 0)
 		return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
+	{
 		if ((str[i] != d && str[i + 1] == d) ||
 				(str[i] != d && !str[i + 1]) || str[i + 1] == d)
 			numwords++;
+	}
 	if (numwords == 0)
 		return (NULL);
 	s = malloc((1 + numwords) * sizeof(char *));
 	if (!s)
 		return (NULL);
 	for (i = 0, j = 0; j < numwords; j++)
+	{
 		while (str[i] == d && str[i] != d)
 			i++;
 		k = 0;
@@ -77,13 +86,16 @@ char **strtow2(char *str, char d)
 			k++;
 		s[j] = malloc((k + 1) * sizeof(char));
 		if (!s[j])
+		{
 			for (k = 0; k < j; k++)
 				free(s[k]);
 			free(s);
 			return (NULL);
+		}
 		for (m = 0; m < k; m++)
 			s[j][m] = str[i++];
 		s[j][m] = 0;
+	}
 	s[j] = NULL;
 	return (s);
 }
